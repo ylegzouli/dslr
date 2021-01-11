447e3b6e692ec44203446b904caf575c748cd1d9
mac <mac@iMac-de-mac.local>
2021-01-12 00:19:51 +0100

    test

--- logreg_train.py
+++ logreg_train.py
@@ -6,15 +6,16 @@ from describe import init_dataset
 #---------------------------------------------------------------------
 
 def     set_data(data_path):
+ 
     data = init_dataset(data_path)
     features = data[1:, 6:]
     features = features.astype(np.float128)
-    # np.where(np.isfinite(features), features, 0)
-    # features = features[~np.isnan(features)]
     targets = data[1:, 1]
     targets[(targets != 'Gryffindor')] = 0. 
     targets[(targets == 'Gryffindor')] = 1.
     targets = targets.astype(np.float128) 
+     # np.where(np.isfinite(features), features, 0)
+    # features = features[~np.isnan(features)]
     
     # print ("features: ", features)
     # print ("features.len: ", len(features))
@@ -27,8 +28,8 @@ def     set_data(data_path):
 
 
 def     init_variables():
+
     weights = np.random.normal(size=13)
-    # weights = weights.astype(np.float128)
     bias = 0
 
     # print ("weights: ", weights)
@@ -39,6 +40,7 @@ def     init_variables():
 
 
 def     pre_activation(features, weights, bias):
+
     z = np.dot(features, weights) + bias
     z = z.astype(np.float128)
     
@@ -52,6 +54,7 @@ def     pre_activation(features, weights, bias):
 
 
 def     activation(z):
+
     y = 1.0 / (1.0 + np.exp(-z))
     
     # print ("z: ", z)
@@ -61,6 +64,7 @@ def     activation(z):
 
 
 def     cost(features, targets, weithts, bias):
+
     m = len(features)
     z = pre_activation(features, weights, bias)
     y = activation(z)
@@ -69,55 +73,50 @@ def     cost(features, targets, weithts, bias):
     cost = (-(1 / m) * np.nansum(targets * np.log(y) + (1 - targets) * np.log(1 - y)))
     
     # print ("features: ", features)
-    # print ("___________________________________________________")
     # print ("targets: ", targets)
-    # print ("___________________________________________________")
     # print ("weights: ", weights)
-    # print ("___________________________________________________")
     # print ("bias: ", bias)
-    # print ("___________________________________________________")
     # print ("z: ", z)
-    # print ("___________________________________________________")
     # print ("y: ", y)
-    # print ("___________________________________________________")
     # print ("m: ", m)
-    # print ("___________________________________________________")
     # print ("cost: ", cost)
-    # print ("___________________________________________________")
 
     return (cost)
 
 
-def     derivative_cost(features, targets, weithts, bias):
+def     gradient(features, targets, weithts, bias):
+
     m = len(features)
     z = pre_activation(features, weights, bias)
     y = activation(z)
-    grad = 1 / m * np.dot(features.transpose(), (y - targets))
-    
+    tr_features = features.transpose()
+    diff = y - targets
+    grad = 1 / m * np.nansum(tr_features * diff)
+
     # print ("features: ", features)
-    # print ("features.len: ", len(features))
+    # print ("m: ", m)
     # print ("targets: ", targets)
     # print ("weights: ", weights)
     # print ("bias: ", bias)
     # print ("z: ", z)
     # print ("y: ", y)
-    # print ("grad: ", grad)
+    # print ("gradient: ", grad)
 
     return (grad)
 
 
 def     train(features, target, weights, bias):
     
-    
-    # print ("features: ", features)
-    # print ("targets: ", targets)
-    # print ("weights: ", weights)
-    # print ("bias: ", bias)
-    # print ("z: ", z)
-    print ("y: ", activation(z))
-    # print ("m: ", m)
+    print ("features: ", features)
+    print ("targets: ", targets)
+    print ("weights: ", weights)
+    print ("bias: ", bias)
+    print ("z: ", pre_activation(features, weights, bias))
+    print ("y: ", activation(pre_activation(features, weights, bias)))
     print ("cost :", cost(features, targets, weights, bias))
-    
+    print ("gradient :", gradient(features, targets, weights, bias))
+    print("Accuracy: ", np.mean(np.round(activation(pre_activation(features, weights, bias)) == targets))) 
+
     return
 
 
@@ -127,5 +126,3 @@ if __name__ == '__main__':
     features, targets = set_data("dataset_train.csv")
     weights, bias = init_variables()
     train(features, targets, weights, bias)
-    
-    # print (derivative_cost(features, targets, weights, bias))
