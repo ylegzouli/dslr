1dee80f4617be097a982751eb525494844079c64
ylegzouli <ylegzoul@student.42.fr>
2021-01-11 01:43:29 +0100

    pb descente de gradient ? activation tjr == 1 -> derive tjr == 0  -> weight_gradient tjr == 0 -> aucune modif des poids.. c chelou

--- logreg_train.py
+++ logreg_train.py
@@ -7,39 +7,25 @@ from describe import init_dataset
 
 def     set_data(data_path):
     data = init_dataset(data_path)
-    
     features = data[1:, 6:]
     features = features.astype(np.float128)
-    
+    # np.where(np.isfinite(features), features, 0)
+    # features = features[~np.isnan(features)]
     targets = data[1:, 1]
     targets[(targets != 'Gryffindor')] = 0. 
     targets[(targets == 'Gryffindor')] = 1.
     targets = targets.astype(np.float128) 
-    
-    # print ("features ----> ", features[:, 0])
-    # print ("features ----> ", features[:, 1])
-    # print ("features ----> ", features)
-    # print ("targets ----> ", targets)
-    # print (targets.shape)
-    # print (features.shape)
-    # plt.scatter(features[:, 1], features[:, 2], c=targets)
-    # plt.show()
     return (features, targets)
 
 
 def     init_variables():
-    # weights = np.zeros((13, 1))
     weights = np.random.normal(size=13)
     weights = weights.astype(np.float128)
     bias = 0
-
-    # print (weights, bias)
     return (weights, bias)
 
 
 def     cost(predictions, targets):
-    # print (predictions, targets)
-    # print ("ici", (predictions - targets) ** 2)
     ret = np.nanmean((predictions - targets) ** 2)
     return ret
 
@@ -60,6 +46,7 @@ def     derivative_activation(z):
 
 def     predict(features, weights, bias):
     z = pre_activation(features, weights, bias)
+
     y = activation(z)
     return np.round(y)
 
@@ -75,25 +62,23 @@ def     train(features, target, weights, bias):
     for epoch in range(epochs):
         if epoch % 10 == 0:
             predictions = activation(pre_activation(features, weights, bias))
-            predictions = predictions.astype(np.float128)
-            # print("Predictions = %s" % predictions)
-            # print("Current cost = %s" % cost(predictions, targets))
-        
+            # print("weights: ", weights)
+            # print("predictions: ", predictions)
         weights_gradients = np.zeros(weights.shape)
-        # weights_gradients = weights_gradients.astype(np.float128)
-        print ("gradient: ", weights_gradients)
         bias_gradient = 0.
         for feature, target in zip(features, targets):
-            z = pre_activation(feature, weights, bias)
-            y = activation(z)
-            print (z)
-            weights_gradients += (y - target) * derivative_activation(z) * feature
-            bias_gradient += (y - target) * derivative_activation(z)
+            if not np.isnan(feature).any():
+                z = pre_activation(feature, weights, bias)
+                y = activation(z)
+                print("test:  !!!!!!!!!!!!!!", y)
+                weights_gradients += (y - target) * derivative_activation(z) * feature
+                # print("test:  !!!!!!!!!!!!!!", derivative_activation(z))
+                bias_gradient += (y - target) * derivative_activation(z)
         weights = weights - (learning_rate * weights_gradients)
         bias = bias - (learning_rate * bias_gradient)
-    
     predictions = predict(features, weights, bias)
-    print(predictions)
+    print("weights: ", weights)
+    print("predictions: ", predictions)
     print("Accuracy = %s" % np.mean(predictions == targets))
     return
 
