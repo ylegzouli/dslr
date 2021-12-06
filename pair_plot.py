05afc82486f557d4b06123141cc0c61ab98aac82
Yamin Legzouli <ylegzoul@student.42.fr>
2021-12-06 15:31:07 +0100

    add pair plot

--- pair_plot.py
+++ pair_plot.py
@@ -1,10 +1,18 @@
-import numpy as np
 import pandas as pd
+import seaborn as sns
 from sys import argv
 import matplotlib.pyplot as plt
 
-def     main():
+def pair_plot():
+    if not len(argv) > 1:
+        print('Please input the data path as first argument.')
+        return
+    try:
+        df = pd.read_csv(argv[1], index_col = "Index")
+        sns.pairplot(df, hue="Hogwarts House")
+        plt.show()
+    except:
+        print('File error')
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    pair_plot()
